10957650eaa018b580566cf3510ffae7a61a88a9
liuyong <liuyong@startimes.com.cn>
2020-03-17 18:12:58 +0800

    Eureka:03171812

--- eureka-server/src/main/java/com/lab/EurekaApplication.java
+++ eureka-server/src/main/java/com/lab/EurekaApplication.java
@@ -2,6 +2,8 @@ package com.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  * CustomerApplication
  * SpringBootApplication默认扫描CustomerApplication所在包所以需要scanBasePackages制定包名
  */
-@RestController
-@SpringBootApplication(scanBasePackages = {"com.lab"})
+@EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
-    }
-
-    @RequestMapping("/ping")
-    public String index() {
-        return "alive";
+        new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
     }
 }
