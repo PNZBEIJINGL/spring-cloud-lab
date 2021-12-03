3da9f5306baac379659e569c9cdc9bd74fde0856
liuyong <liuyong@startimes.com.cn>
2021-12-03 11:26:50 +0800

    202112031126:ZUUL

---
+++ api-gateway/src/main/java/com/springcloud/gateway/filter/AccessFilter.java
@@ -0,0 +1,42 @@
+package com.springcloud.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        Object accessTocken = request.getParameter("accessTocken");
+        if (accessTocken == null) {
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+            return null;
+        }
+        return null;
+    }
+}
