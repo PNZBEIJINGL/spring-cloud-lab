40db6bf962815782f0048148ee2ecf5ec766eb52
liuyong <liuyong@startimes.com.cn>
2022-01-05 19:29:01 +0800

    01051928:STREAM

---
+++ stream-rabbitmq/src/main/java/com/springcloud/stream/receiver/SinkReceiver.java
@@ -0,0 +1,22 @@
+package com.springcloud.stream.receiver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+public class SinkReceiver {
+
+    private static Logger Log = LoggerFactory.getLogger(SinkReceiver.class);
+
+    /**
+     *
+     * @param payload 有效载荷,一般指对于接收者有用的数据
+     */
+    @StreamListener(Sink.INPUT)
+    private void receive(Object payload) {
+        Log.info("SinkReceiver:" + payload);
+    }
+}
